96a905179b8f23394fcba22d2eb048bb38a542d8
Mike Kamermans <pomax@nihongoresources.com>
2013-07-16 11:32:36 -0700

    now with SSO

--- app.js
+++ app.js
@@ -14,8 +14,18 @@ var app = express(),
 
 // make it small.
 app.use(express.compress());
-app.use(express.static(path.join(__dirname, 'webxray/static-files')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.cookieSession({
+  key: "goggles.sid",
+  secret: env.get("SESSION_SECRET"),
+  cookie: {
+    maxAge: 2678400000, // 31 days. Persona saves session data for 1 month
+    secure: !!env.get("FORCE_SSL")
+  },
+  proxy: true
+}));
+app.use(express.csrf());
 app.use(function(err, req, res, next) {
   console.log("error: ", err);
   res.render(500);
@@ -24,13 +34,36 @@ app.use(function(err, req, res, next) {
 // Enable template rendering with nunjucks
 nunjucksEnv.express(app);
 
+// used for webxray .js source files
 app.param("filename", function(req, res, next, filename) {
   req.params.filename = filename;
   next();
 })
+
+// reroute webxray .js source files when requested
 app.get("/src/:filename", function(req, res) {
   res.render("./webxray/src/"+req.params.filename);
 })
+
+// intercept webxray's index.html and use our own
+app.get("/", function(req, res) {
+  res.render("index.html", {
+    login: env.get("login"),
+    audience: env.get("audience"),
+    email: req.session.email || "",
+    csrf: req.session._csrf || ""
+  });
+});
+
+// serve static content, resolved in this order:
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'webxray/static-files')));
+
+// login API connections
+require('webmaker-loginapi')(app, {
+  loginURL: env.get('LOGINAPI'),
+  audience: env.get('AUDIENCE')
+});
 
 // run server
 goggles.build(env, nunjucksEnv, function() {
