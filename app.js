524cc8e24e37283fcfe64af879444cf72b3afb5c
Jon Buckley <jon@jbuckley.ca>
2013-12-02 12:43:39 -0500

    Fix bug 945321 - Add option to enable HSTS

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ app.locals({
   listDropdownLang: i18n.getSupportLanguages()
 });
 
+if (!!env.get("FORCE_SSL") ) {
+  app.use(helmet.hsts());
+  app.enable("trust proxy");
+}
 app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 app.use(express.compress());
 app.use(express.json());
