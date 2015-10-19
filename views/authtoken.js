bd10d4ec69f2ea245ad96fda571d22d9d487aae5
Pomax <pomax@nihongoresources.com>
2015-10-19 14:16:40 -0700

    don't wipe auth tokens

--- views/authtoken.js
+++ views/authtoken.js
@@ -1,13 +1,15 @@
 (function() {
   var tks = location.hash;
+
   if (tks) {
     tks = tks.split("#token=");
     if (tks[1]) {
       localStorage["goggles-auth-token"] = tks[1];
-    } else { localStorage.removeItem("goggles-auth-token"); }
-  } else { localStorage.removeItem("goggles-auth-token"); }
+    } // else { localStorage.removeItem("goggles-auth-token"); }
+  } // else { localStorage.removeItem("goggles-auth-token"); }
 
   if (window.location.toString().indexOf("logout=true") !== -1) {
+    localStorage.removeItem("goggles-auth-token");
     var loginStatus = document.querySelector(".loginstatus")
     if (loginStatus) {
       loginStatus.textContent = "out";
