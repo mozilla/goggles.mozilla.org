55ad4820592afaa764da0d55ef14472da14344a0
Pomax <pomax@nihongoresources.com>
2015-10-20 10:51:18 -0700

    chrome test

--- app.js
+++ app.js
@@ -35,8 +35,10 @@ nunjucksEnv.addFilter( "instantiate", function( input ) {
     return tmpl.render( this.getVariables() );
 });
 
-var forceSSL = !!env.get("FORCE_SSL");
-
+if (!!env.get("FORCE_SSL") ) {
+  app.use(helmet.hsts());
+  app.enable("trust proxy");
+}
 
 app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 app.use(express.compress());
