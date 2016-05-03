e78732332a16034a5368a5624be0cdbdd510fdb9
Pomax <pomax@nihongoresources.com>
2016-05-03 15:14:17 -0700

    linking to SUMO

--- views/gogglesnotice.js
+++ views/gogglesnotice.js
@@ -35,6 +35,27 @@
     link.setAttribute("style", "color:white!important;text-decoration:underline!important;");
     notice.appendChild(span);
     notice.appendChild(link);
+  } else {
+    var span = document.createElement("span");
+    span.innerHTML = " - If this looks broken click ";
+    notice.appendChild(span);
+    link = document.createElement("a");
+    link.href = window.location.toString().replace("https://","http://");
+    link.textContent = "here";
+    link.setAttribute("style", "color:white!important;text-decoration:underline!important;");
+    notice.appendChild(link);
+    span = document.createElement("span");
+    span.innerHTML = " to go back to the HTTP version, but also click ";
+    notice.appendChild(span);
+    link = document.createElement("a");
+    link.href = "https://support.mozilla.org/en-US/kb/why-do-i-see-message-asking-if-i-want-see-page-hos";
+    link.setAttribute("target", "_blank");
+    link.textContent = "here";
+    link.setAttribute("style", "color:white!important;text-decoration:underline!important;");
+    notice.appendChild(link);
+    span = document.createElement("span");
+    span.innerHTML = " to read more about why this happens.";
+    notice.appendChild(span);
   }
 
   var closer = document.createElement("div");
